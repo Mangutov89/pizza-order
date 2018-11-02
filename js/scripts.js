c5988026dcfa153912f007dd62027a7478771d32
lm <leo.mangutov@gmail.com>
2018-11-02 13:19:21 -0700

    Add pizza topping calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 //
-function Pizza (pizzaTopping, pizzaSize) {
+function Pizza (userName, pizzaTopping, pizzaSize) {
+  this.name = userName
   this.pizzaTopping = pizzaTopping,
   this.pizzaSize = pizzaSize,
   this.price = 20;
 }
 
+Pizza.prototype.calculatePizzaTopping = function() {
+  if (this.pizzaTopping === "Cheese") {
+    this.price -= 4;
+  } else if (this.pizzaTopping === "Pepperoni") {
+    this.price -= 2;
+  } else if (this.pizzaTopping === "Meat lovers") {
+    this.price += 2;
+  } else {
+    this.price += 4;
+  }
+
+}
 
 
 
