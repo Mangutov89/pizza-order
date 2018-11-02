e9a16b318cf2814bf5dc2a65b559bebeb8f00292
lm <leo.mangutov@gmail.com>
2018-11-02 11:37:53 -0700

    forgot to save file before commiting

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//
+function Pizza (pizzaTopping, pizzaSize) {
+  this.pizzaTopping = pizzaTopping,
+  this.pizzaSize = pizzaSize,
+  this.price = 20;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UI
+$(document).ready(function() {
+  $("#pizzaorder").submit(function(event) {
+    event.preventDefault();
+
+
+
+
+
+
+
+
+
+
+
+  });
+});
